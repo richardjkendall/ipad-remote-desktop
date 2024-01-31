2783bcbecc21a74144711c94fe75bebae390de50
Richard Kendall <richard.kendall@gmail.com>
2024-01-31 20:13:39 +1100

    basic version no proper auth is working now

--- remotedesktop/ContentView.swift
+++ remotedesktop/ContentView.swift
@@ -13,6 +13,8 @@ struct GuacViewer: UIViewRepresentable {
     let webView: WKWebView
     let hostName: String
     
+    let messageHandler = MessageHandler()
+    
     init(host: String) {
         print("Loading webview for \(host)")
         
@@ -27,6 +29,16 @@ struct GuacViewer: UIViewRepresentable {
         let wkUserScript = WKUserScript(source: userScript, injectionTime: WKUserScriptInjectionTime.atDocumentStart, forMainFrameOnly: false)
         userContentController.addUserScript(wkUserScript)
         webConfig.userContentController = userContentController
+        webConfig.userContentController.add(messageHandler, name: "console")
+        
+        let authCookie = HTTPCookie(properties: [
+            .domain: "localhost",
+            //.port: "8080",
+            .path: "/",
+            .name: "X-Remote-User",
+            .value: "rjk"
+        ])!
+        webConfig.websiteDataStore.httpCookieStore.setCookie(authCookie)
         
         webView = WKWebView(frame: .zero, configuration: webConfig)
     }
@@ -39,13 +51,20 @@ struct GuacViewer: UIViewRepresentable {
     func updateUIView(_ uiView: WKWebView, context: Context) {
         if let url = Bundle.main.url(forResource: "viewer", withExtension: "html") {
             print("inside updateUIView for \(hostName)")
+            print("file \(url) path \(url.deletingLastPathComponent())")
             webView.loadFileURL(url, allowingReadAccessTo: url.deletingLastPathComponent())
         } else {
             print("HTML file not found")
         }
     }
     
-    
+    class MessageHandler: NSObject, WKScriptMessageHandler {
+        func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+            if message.name == "console" {
+                print("js console: \(message.body)")
+            }
+        }
+    }
     
 }
 
